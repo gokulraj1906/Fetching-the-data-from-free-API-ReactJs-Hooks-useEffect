a5c73dd54928245ec39b46bc7c8f1f91960d001d
gokulraj1906 <gokulraj902580@gmail.com>
2024-08-17 12:32:06 +0530

    Initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React,{useState,useEffect} from 'react';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Welcome(){
+  const[users,setUsers]=useState();
+  const[loading,setLoading]=useState(true);
+  useEffect(()=>{
+  fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => 
+      {
+        setUsers(data);
+        setLoading(false);
+      }
+      )
+      .catch(error =>{
+        console.log("Error");
+        setLoading(false);
+      });
+    });
+  if(loading){
+    return  <p>Loading...</p>
+  }
+  return(
+    <div>
+    <ol>
+      {users.map(users=>(
+        <li key={users.id} >{users.name},{users.email}, {users.address.zipcode}</li>
+      ))}
+      </ol>
+      </div>
+  )
+}
+ReactDOM.render(<Welcome/>,document.getElementById("root"));
